d53afb5398fb78a269d878a7cc7544a48860d5c6
1marcomachado <71024238+1marcomachado@users.noreply.github.com>
2025-04-21 15:13:52 +0100

    Update jsonscript.js

--- jsonscript.js
+++ jsonscript.js
@@ -1,5 +1,5 @@
 let quizData;
-const jsonUrl = "https://cdn.jsdelivr.net/gh/1marcomachado/teste@13.00/jsonformatter.json";
+const jsonUrl = "https://cdn.jsdelivr.net/gh/1marcomachado/teste@15.11/jsonformatter.json";
 
 fetch(jsonUrl)
   .then(res => res.json())
