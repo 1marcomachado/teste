85703eeed5fba5b3f665ba9c91e30276680619ee
1marcomachado <71024238+1marcomachado@users.noreply.github.com>
2025-08-06 15:17:39 +0100

    Update carrinho-upselling.js

--- carrinho-upselling.js
+++ carrinho-upselling.js
@@ -114,7 +114,8 @@ window.addEventListener("load", () => {
     .upselling-carousel .sizes-list {
       display: none;
       position: absolute;
-      inset: 6rem 0 0 0;
+      top: 6rem;
+      inset: 0 0 0 0;
       background-color: rgba(255, 255, 255, 0.9);
       z-index: 999;
       padding: 0 20px;
