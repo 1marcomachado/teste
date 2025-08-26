ec3647ad88096fc5368e1a4bb73361b7532ffe11
1marcomachado <71024238+1marcomachado@users.noreply.github.com>
2025-08-26 09:44:55 +0100

    Update add-to-cart-sticky.js

--- add-to-cart-sticky.js
+++ add-to-cart-sticky.js
@@ -51,7 +51,7 @@
     margin: '0',
     borderRadius: '0',
     display: 'none',
-    height: '55px',
+    height: '58px',
     padding: '20px',
     borderColor: '#363636',
     background: '#363636',
